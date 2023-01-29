a065b17b72ef0ed3533a02b48ffcd59f1f901554
manuelmad <manuelmad1981@gmail.com>
2023-01-29 18:44:34 -0400

    Realizando curso de webpack clase 24

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <h1>Helllo Reac!!!!!!</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js' // No tiene que ser obligatorio main.js
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'] // Posibes extensiones en React
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3006
+    }
+}
